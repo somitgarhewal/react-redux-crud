b78ab6ec074b77b060528183ddf24abeb8c85c10
somitgarhewal <somitgarhewal@gmail.com>
2020-03-06 14:43:45 +0530

    Home page created

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container-fluid backgroundFooter'>
+      <div className='row'>
+        <div className='col-4 text-center'>
+          Thankyou for visiting the site
+          <br />
+          please visit again
+        </div>
+        <div className='col-4'></div>
+        <div className='col-4 text-center'>
+          Thankyou for visiting the site
+          <br />
+          please visit again
+        </div>
+      </div>
+    </div>
+  )  
+}
+
+export default Footer
---
+++ src/components/main_components/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <h1 className='text-center'>Home page</h1>
+    )
+  }
+}
+export default Home
